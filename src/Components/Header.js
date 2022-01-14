721acacde84e32d5d43d84fe4050e6c92fbfbdbe
Staff Asia <staffasia@Staffs-iMac.local>
2022-01-14 17:12:16 +0600

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 style={{color:'grey'}}>Material UI To Do <span style={{color:'#1976d2'}}>App</span></h1>
+        </div>
+    );
+};
+
+export default Header;
